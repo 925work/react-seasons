a7394a09971388734c8499c19cacd251784835e4
Alex Pham <925workk@gmail.com>
2021-03-08 20:50:34 -0700

    added seasons display component to render depending on users location and time of month.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            lat: 0
-        };
+    state = {lat: null, errorMessage:''};
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
+        );
     }
 
-
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
-        );
-        return <div>Latitude: </div>;
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <div>Loading....</div>
     }
 }
 
